a9d974d6a6ee83431016810ee3346f44e09b4972
yuriy_chistyakov <yuchistyakov@gmail.com>
2024-12-29 21:48:04 +0200

    pass them as a context to RAG

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rhino
+
+## get discord channel messages
+![Discord](https://res.cloudinary.com/dzd5mddlm/image/upload/v1735500458/Discord/ftavcrsljby5a6z20kzj.png)
+## pass them as a context to RAG
+![dRAG](https://res.cloudinary.com/dzd5mddlm/image/upload/v1735500264/Discord/ffmh3cr7ol8ciggjttio.png)
--- assembly/index.ts
+++ assembly/index.ts
@@ -1,8 +1,13 @@
-import { models } from "@hypermode/modus-sdk-as";
-import { EmbeddingsModel } from "@hypermode/modus-sdk-as/models/experimental/embeddings";
+import { models, http } from "@hypermode/modus-sdk-as";
 import { Message } from "./classes";
 import * as console from "as-console";
-import { http } from "@hypermode/modus-sdk-as";
+import {
+  OpenAIChatModel,
+  SystemMessage,
+  UserMessage,
+  Message as AIMessage
+} from "@hypermode/modus-sdk-as/models/openai/chat";
+import { JSON } from "json-as";
 
 export function Discord(q: string, channel_id: string, message_id: string): Message[] {
   const url = `https://discord.com/api/v10/channels/${channel_id}/messages`
@@ -18,11 +23,29 @@ export function Discord(q: string, channel_id: string, message_id: string): Mess
   return response.json<Message[]>()
 }
 
-export function genEmbs(texts: string[]): f32[][] {
-  console.log(texts[0]);
-  const model = models.getModel<EmbeddingsModel>("text-generator");
-  const input = model.createInput(texts);
-  const output = model.invoke(input);
-  return output.predictions;
+const modelName: string = "text-generator"
+
+export function DRAG(instruction: string, q: string, channel_id: string, message_id: string): string/* [] */ {
+  const response = Discord(q, channel_id, message_id);
+  const prompts = response.map<string>((c) => c.content.trim());
+  
+  let messages: AIMessage[] = [new SystemMessage(instruction),]
+
+  for (let i = 0; i < prompts.length; i++) {
+    const prompt = prompts[i];
+    if (prompt.length) { console.log(prompt);
+      messages.push(new UserMessage(prompt)); 
+    }
+  }
+
+  const model = models.getModel<OpenAIChatModel>(modelName)
+  const input = model.createInput(messages)
+
+  // this is one of many optional parameters available for the OpenAI chat interface
+  input.temperature = 0.7
+
+  const output = model.invoke(input)
+  console.log(JSON.stringify(output.choices));
+  return output.choices[0].message.content.trim()
 }
 
